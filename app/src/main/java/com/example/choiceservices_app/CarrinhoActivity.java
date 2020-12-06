10d140fcc166997708385f9d2bc949e725ddbc22
gabid <gabriela.netor94@gmail.com>
2020-12-05 23:43:08 -0300

    cart

--- app/src/main/java/com/example/choiceservices_app/CarrinhoActivity.java
+++ app/src/main/java/com/example/choiceservices_app/CarrinhoActivity.java
@@ -3,8 +3,36 @@ package com.example.choiceservices_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarrinhoActivity extends AppCompatActivity {
+
+    private ListView lvLista;
+    private List<Servico> listaDeServicos;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    private ChildEventListener childEventListener;
+    private Query query;
+    private Menu binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
